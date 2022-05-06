fa4ef238ba193b3843c737b60a5414288787efb5
EthanP94 <ethan.plott@yahoo.com>
2022-05-05 22:05:49 -0700

    added file structure and some coding to the

---
+++ src/componets/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about-me">
+      <h1>About Me</h1>
+      <p>
+      I was born and raised in Southern California and grew up in sunny Palm Springs California. I found my love coding and computers and decided to take the leap and make it my career. I am currently a student 
+      enrolled in the UCLA Extension Coding Bootcamp.
+        <span>
+          <a href=""></a>
+        </span>
+        or
+        <span>
+          <a href="">
+          </a>
+        </span>
+        <span>
+          <a href=""></a>
+        </span>
+      </p>
+    </div>
+  );
+}
---
+++ src/componets/pages/Contact.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <div className="contact-me">
+      <h1>Contact Me Here!</h1>
+      <ul>
+        <li>
+          <a href="https://www.linkedin.com/in/ethan-plott-045232238/">LinkedIn</a>
+        </li>
+        <li>
+          <a href="mailto:ethan.plott@yahoo.com">Email</a>
+        </li>
+        <li>
+          <a href="https://github.com/EthanP94">Github</a>
+        </li>
+        <li>
+          <a href="https://docs.google.com/document/d/11he9FMj1uM2phs1uVD8-Rl9oz-D2p85vybyNmm7iuWc/edit?usp=sharing" download="Resume">
+            Resume
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
