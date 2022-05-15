737fed5b0f2c0ac1780c3989057c7fb16fddeecf
EthanP94 <ethan.plott@yahoo.com>
2022-05-15 10:23:56 -0700

    more changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,8 +5,7 @@ export default function About() {
     <div className="about-me">
       <h1>About Me</h1>
       <p>
-      I was born and raised in Southern California and grew up in sunny Palm Springs California. I found my love coding and computers and decided to take the leap and make it my career. I am currently a student 
-      enrolled in the UCLA Extension Coding Bootcamp.
+      I was born and raised in Southern California and grew up in sunny Palm Springs California. I found my love coding and computers and decided to take the leap and make it my career.
       </p>
     </div>
   );
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,14 +1,19 @@
 import React from "react";
-import backgroundImage from '../../Images/background.png';
+// import backgroundImage from '../../Images/background.png';
 
 export default function Home() {
   return (
     <div>
       <h1>Ethan Plott Portfolio</h1>
-      <img
+      {/* <img
         src={backgroundImage}
         alt={"One Piece"}
         className="hero"
+      /> */}
+       <img
+        src={require("../../Images/background.png")}
+        alt={"One Piece"}
+        className="background"
       />
     </div>
   );
